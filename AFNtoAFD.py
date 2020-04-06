49a16942c94319d458973f788c91e9f21fc58086
Constantin Gabriel <constantin.gab02@gmail.com>
2020-04-06 22:04:20 +0300

    work in progress

--- AFNtoAFD.py
+++ AFNtoAFD.py
@@ -29,5 +29,29 @@ for i in range(1,maxim_st+1):
     print('')
 print(start)
 print(final)
+parcurs=[] #vector in care retinem nodurile prin care am trecut
+b[1][1]=a[1][1]
+b[1][2]=a[1][2]
+parcurs.append(a[1][1])
+parcurs.append(a[1][2])
+poz=0   #variabila cu care determin litera de legatura din alfabet
+for x in parcurs:
+    curent=''
+    if len(x)>1:
+        ind=0
+        while(ind<len(x)):
+            if a[int(x[ind])][poz % maxim_lit+1]!='0' and a[int(x[ind])][poz % maxim_lit+1] not in curent:
+                curent=curent+a[int(x[ind])][poz % maxim_lit]
+            ind+=1
+        curent=''.join(sorted(curent))
+    elif a[int(x)][poz % maxim_lit+1]!='0':
+        curent=a[int(x)][poz % maxim_lit+1]
+    #print(curent,int(x),poz % maxim_lit+1)
+    if curent!='' and curent not in parcurs:
+        parcurs.append(curent)
+    poz+=1
+print(parcurs)
+
+
 #for i in range(1,maxim_st+1):
     #for j in range(1,maxim_lit+1):
